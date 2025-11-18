0c6a912e5c8fbb924802353727c7a0f2e5a5a089
André Vicente Gonçalves <andre@andrevicentegoncalves.com>
2025-11-18 09:49:20 +0000

    updating style guide

--- src/pages/style-guide/sections/component-sections/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Alerts } from './Alerts.jsx';
-export { default as Badges } from './Badges.jsx';
-export { default as Buttons } from './Buttons.jsx';
-export { default as Cards } from './Cards.jsx';
-export { default as Dropdowns } from './Dropdowns.jsx';
-export { default as Dividers } from './Dividers.jsx';
-export { default as FormAndInputs } from './FormAndInputs.jsx';
-export { default as Modals } from './Modals.jsx';
